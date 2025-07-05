e27a9864a0d71253fd898ae5ebed82c0b2acb16c
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-07-05 20:13:48 +0200

    env for prisma

--- src/shells/default.nix
+++ src/shells/default.nix
@@ -61,6 +61,11 @@
             };
           })
         ];
+        env = {
+          PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
+          PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
+          PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
+        };
       };
       rust = pkgs.mkShell {
         buildInputs = with pkgs; [
