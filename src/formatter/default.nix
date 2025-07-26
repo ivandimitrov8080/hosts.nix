738f71f55091eb562c74981c032d9bfd5f91056d
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-07-27 00:36:22 +0200

    formatter config

--- src/formatter/default.nix
+++ src/formatter/default.nix
@@ -1,7 +1,27 @@
 { inputs }:
 let
   system = "x86_64-linux";
+  pkgs = import inputs.nixpkgs { inherit system; };
 in
 {
-  "${system}" = inputs.nixpkgs.legacyPackages.${system}.nixfmt-tree;
+  "${system}" = pkgs.treefmt.withConfig {
+    runtimeInputs = with pkgs; [
+      nixfmt-rfc-style
+      shfmt
+    ];
+
+    settings = {
+      # Log level for files treefmt won't format
+      on-unmatched = "info";
+
+      # Configure nixfmt for .nix files
+      formatter = {
+        nixfmt.command = "nixfmt";
+        nixfmt.includes = [ "*.nix" ];
+        sh.command = "shfmt";
+        sh.options = [ "--write" ];
+        sh.includes = [ "*.sh" ];
+      };
+    };
+  };
 }
