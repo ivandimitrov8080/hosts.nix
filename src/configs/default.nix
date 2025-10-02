01a57ec4a06039b3d93ff2ae1fd28caa4473b683
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-10-02 09:09:34 +0200

    migrate con to here

--- src/configs/default.nix
+++ src/configs/default.nix
@@ -629,6 +629,142 @@ let
             };
           };
         };
+      mail =
+        {
+          config,
+          lib,
+          pkgs,
+          options,
+          ...
+        }:
+        let
+          inherit (lib) mkIf mkEnableOption optionalAttrs;
+          cfg = config.meta.mail;
+          hasMailserver = options ? mailserver;
+        in
+        {
+          options.meta.mail = {
+            enable = mkEnableOption "enable mailserver config";
+          };
+          config = mkIf cfg.enable (
+            optionalAttrs hasMailserver {
+              mailserver = {
+                stateVersion = 3;
+                enable = true;
+                localDnsResolver = false;
+                fqdn = "idimitrov.dev";
+                domains = [
+                  "idimitrov.dev"
+                  "mail.idimitrov.dev"
+                ];
+                loginAccounts = {
+                  "ivan@idimitrov.dev" = {
+                    hashedPassword = "$2b$05$rTVIQD98ogXeCBKdk/YufulWHqpMCAlb7SHDPlh5y8Xbukoa/uQLm";
+                    aliases = [ "admin@idimitrov.dev" ];
+                  };
+                  "security@idimitrov.dev" = {
+                    hashedPassword = "$2b$05$rTVIQD98ogXeCBKdk/YufulWHqpMCAlb7SHDPlh5y8Xbukoa/uQLm";
+                  };
+                };
+                certificateScheme = "acme-nginx";
+                hierarchySeparator = "/";
+              };
+              services = {
+                roundcube = {
+                  enable = true;
+                  package = pkgs.roundcube.withPlugins (plugins: [ plugins.persistent_login ]);
+                  plugins = [
+                    "persistent_login"
+                  ];
+                  hostName = "mail.idimitrov.dev";
+                  extraConfig = ''
+                    $config['imap_host'] = 'ssl://${config.mailserver.fqdn}:993';
+                    $config['smtp_host'] = "ssl://${config.mailserver.fqdn}";
+                    $config['smtp_user'] = "%u";
+                    $config['smtp_pass'] = "%p";
+                  '';
+                };
+                nginx.virtualHosts =
+                  let
+                    restrictToVpn = ''
+                      allow 127.0.0.1/32;
+                      allow 10.0.0.2/32;
+                      allow 10.0.0.3/32;
+                      allow 10.0.0.4/32;
+                      allow 10.0.0.5/32;
+                      deny all;
+                    '';
+                  in
+                  {
+                    "mail.idimitrov.dev" = {
+                      extraConfig = restrictToVpn;
+                    };
+                  };
+                postgresql.enable = true;
+              };
+              security = {
+                acme = {
+                  acceptTerms = true;
+                  defaults.email = "security@idimitrov.dev";
+                };
+              };
+            }
+          );
+        };
+      nginx = {
+        services.nginx.virtualHosts = {
+          "src.idimitrov.dev" = {
+            enableACME = true;
+            forceSSL = true;
+            locations."/" = {
+              proxyPass = "http://127.0.0.1:9418";
+              extraConfig = ''
+                allow 10.0.0.0/8;
+                allow 192.168.0.0/8;
+                deny all;
+                proxy_http_version 1.1;
+                proxy_set_header Upgrade $http_upgrade;
+                proxy_set_header Connection "upgrade";
+              '';
+            };
+          };
+          "pic.idimitrov.dev" = {
+            enableACME = true;
+            forceSSL = true;
+            locations."/" = {
+              root = "/var/pic";
+              extraConfig = ''
+                autoindex on;
+              '';
+            };
+          };
+          "ai.idimitrov.dev" = {
+            enableACME = true;
+            forceSSL = true;
+            locations."/" = {
+              proxyPass = "http://10.0.0.4:8080";
+              extraConfig = ''
+                allow 10.0.0.0/8;
+                allow 192.168.0.0/8;
+                deny all;
+                proxy_http_version 1.1;
+                proxy_set_header Upgrade $http_upgrade;
+                proxy_set_header Connection "upgrade";
+              '';
+            };
+          };
+        };
+        services.postgresql.ensureUsers = [
+          {
+            name = "root";
+            ensureClauses = {
+              superuser = true;
+              createrole = true;
+              createdb = true;
+            };
+          }
+        ];
+      };
     };
   hardwareConfigurations = import ../constants;
   configWithModules =
@@ -674,8 +810,10 @@ let
     configWithModules {
       modules =
         (with nixosModules; [
-          default
           vpsadminosModule
+          default
+          mail
+          nginx
         ])
         ++ mods;
     };
