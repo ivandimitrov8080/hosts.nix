4fd2db1a12167e309b88f0ea4034c7b1967d922f
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-11-01 17:13:00 +0100

    packages: add java plugins

--- src/packages/default.nix
+++ src/packages/default.nix
@@ -1,8 +1,18 @@
 { inputs, system }:
 let
-  pkgs = import inputs.nixpkgs { inherit system; };
+  overlay = (import ../overlays { inherit inputs; }).default;
+  config = (import ../overlays { inherit inputs; }).config;
+  pkgs = import inputs.nixpkgs {
+    inherit system;
+    overlays = [
+      overlay
+      config
+    ];
+  };
 in
 {
   swhkd = pkgs.callPackage ./swhkd { };
   ndlm = pkgs.callPackage ./ndlm { };
+  vscode-java-debug = pkgs.vscode-java-debug;
+  vscode-java-test = pkgs.vscode-java-test;
 }
