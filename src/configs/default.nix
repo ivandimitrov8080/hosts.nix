eb9d97ce01960e4f1915ff723a5bd1d4f8dc2f9a
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2026-02-16 15:26:03 +0100

    steam desktop entries generator service

--- src/configs/default.nix
+++ src/configs/default.nix
@@ -698,24 +698,6 @@ rec {
     modules = [
       (
         { pkgs, ... }:
-        let
-          desktopItems = [
-            (pkgs.makeDesktopItem {
-              name = "dota";
-              desktopName = "DotA 2";
-              exec = "${pkgs.steam}/bin/steam -silent steam://launch/570/dialog";
-              terminal = false;
-              icon = "steam_icon_570";
-            })
-            (pkgs.makeDesktopItem {
-              name = "cs2";
-              desktopName = "Counter Strike 2";
-              exec = "${pkgs.steam}/bin/steam -silent steam://launch/730/dialog";
-              terminal = false;
-              icon = "steam_icon_730";
-            })
-          ];
-        in
         {
           meta.gaming.enable = true;
           home-manager.users.ivand = {
@@ -740,7 +722,65 @@ rec {
               };
             };
           };
-          environment.systemPackages = with pkgs; [ radeontop ] ++ desktopItems;
+          systemd.user = {
+            timers = {
+              steam-desktop-entries = {
+                wantedBy = [ "timers.target" ];
+                timerConfig = {
+                  OnCalendar = "*-*-* 10:00:00";
+                  Persistent = true;
+                };
+              };
+            };
+            services.steam-desktop-entries =
+              let
+                exe =
+                  pkgs.writers.writePython3 "steam-desktop-entries"
+                    {
+                      libraries = with pkgs.python3Packages; [ vdf ];
+                    }
+                    # py
+                    ''
+                      import glob
+                      import os
+                      import vdf
+                      steamapps = os.path.expanduser("~/.local/share/Steam/steamapps")
+
+
+                      def save_desktop_entry(appid, name):
+                          appid = str(appid).strip()
+                          name = (name or "").strip()
+                          path = os.path.expanduser(
+                              f"~/.local/share/applications/game-{appid}.desktop"
+                          )
+                          with open(path, "w", encoding="utf-8") as f:
+                              f.write(f"""[Desktop Entry]
+                      Exec=steam -silent steam://launch/{appid}/dialog
+                      Icon=steam_icon_{appid}
+                      Name={name}
+                      Terminal=false
+                      Type=Application
+                      Version=1.5
+                      """)
+
+
+                      for path in sorted(glob.glob(os.path.join(steamapps, "appmanifest_*.acf"))):
+                          with open(path, "r", encoding="utf-8", errors="replace") as f:
+                              data = vdf.load(f)
+                          st = data.get("AppState", {})
+                          appid = st.get("appid")
+                          name = st.get("name")
+                          save_desktop_entry(appid, name)
+                    '';
+              in
+              {
+                description = "Generate desktop files for all installed games";
+                script = ''
+                  ${exe}
+                '';
+              };
+          };
+          environment.systemPackages = with pkgs; [ radeontop ];
         }
       )
     ];
