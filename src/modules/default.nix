8c6cc774f696eaefc71f74b0d3cfb5a60211aaa5
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2026-03-30 17:48:17 +0200

    fix radicale nginx errs

--- src/modules/default.nix
+++ src/modules/default.nix
@@ -852,9 +852,10 @@ in
                     "10.0.0.1"
                   ];
                   locations."/" = {
-                    proxyPass = "http://localhost:5232/";
+                    proxyPass = "http://127.0.0.1:5232/";
                     extraConfig = ''
-                      proxy_set_header  X-Script-Name /;
+                      proxy_set_header Host $host;
+                      proxy_set_header X-Forwarded-Proto $scheme;
                       proxy_set_header  X-Forwarded-For $proxy_add_x_forwarded_for;
                       proxy_pass_header Authorization;
                     '';
