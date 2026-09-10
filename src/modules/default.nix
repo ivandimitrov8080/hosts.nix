bf1e9694cba68a03bbe821b2391ab35d0e4b91e4
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2026-09-10 09:25:19 +0200

    kmonad remap pgup pgdn to left right

--- src/modules/default.nix
+++ src/modules/default.nix
@@ -225,7 +225,33 @@ in
             resolved.enable = true;
             guix.enable = true;
             libretranslate.enable = true;
+            kmonad = {
+              enable = true;
+              keyboards.laptop = {
+                device = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
+                defcfg.enable = true;
+                config = ''
+                  (defsrc
+                    esc  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11  f12  ssrq pause ins del
+                    grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
+                    tab  q    w    e    r    t    y    u    i    o    p    [    ]
+                    caps a    s    d    f    g    h    j    k    l    ;    '    \    ret
+                    lsft z    x    c    v    b    n    m    ,    .    /    rsft       pgup up   pgdn
+                    lctl lmet lalt           spc                 ralt rctl            left down rght
+                  )
+                  (deflayer base
+                    esc  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11  f12  ssrq pause ins del
+                    grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
+                    tab  q    w    e    r    t    y    u    i    o    p    [    ]
+                    caps a    s    d    f    g    h    j    k    l    ;    '    \    ret
+                    lsft z    x    c    v    b    n    m    ,    .    /    rsft       left up   right
+                    lctl lmet lalt           spc                 ralt rctl            left down rght
+                  )
+                '';
+              };
+            };
           };
+          hardware.uinput.enable = true;
           meta = {
             graphicalBoot.enable = true;
             shells.enable = true;
