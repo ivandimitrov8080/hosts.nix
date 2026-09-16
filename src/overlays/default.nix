9a427094003bb940bd401c2c04ed59bd82c4884f
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2026-09-16 05:58:17 +0000

    cleanup morep kgs

--- src/overlays/default.nix
+++ src/overlays/default.nix
@@ -11,12 +11,7 @@
         package = inputs.neovim-nightly-overlay.packages.${stdenv.hostPlatform.system}.default;
       };
       which-key = callPackage ../packages/which-key { };
-      twvtodo = callPackage ../packages/twvtodo { };
-      xsstrike = callPackage ../packages/xsstrike { };
-      myMpvScripts = callPackage ../packages/mpvScripts { };
       xin = callPackage ../packages/xin { };
-      mobile-config-firefox = callPackage ../packages/mobile-config-firefox { };
-      emigo = callPackage ../packages/emigo { };
       llm-tool-collection = callPackage ../packages/llm-tool-collection { };
       docs-nixos = inputs.self.nixosConfigurations.nova.config.system.build.manual.optionsJSON;
       docs-hm = inputs.home-manager.packages.${prev.stdenv.hostPlatform.system}.docs-json;
